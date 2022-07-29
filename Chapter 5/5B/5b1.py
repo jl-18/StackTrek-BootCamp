714cf7997ca009011f0ca13a126051699010bfb6
jl-18 <67959771+jl-18@users.noreply.github.com>
2022-07-29 12:24:09 +0800

    Rocket Countdown-
    Write a countdown code. It starts with a given number (e.g., 10), and counts down to zero, 
    printing each number it encounters (10, 9, 8, ...). It does not print 0, instead it prints “Blast off!”

---
+++ Chapter 5/5B/5b1.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls||clear')
+
+# Code starts here
+
+n = int(input())
+
+counter = n
+for i in range(n):
+  print(n)
+  n -= 1
+print("Blast off!")
+
+#----Rocket Countdown---
+#Write a countdown code. It starts with a given number (e.g., 10), and counts down to zero, 
+# printing each number it encounters (10, 9, 8, ...). It does not print 0, instead it prints “Blast off!”
